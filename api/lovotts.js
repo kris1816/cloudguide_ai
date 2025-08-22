e2407f5da6f075094f284f757189b391e13b8c1d
kris1816 <kris1816@gmail.com>
2025-08-22 14:55:37 +0700

    Update lovotts.js

--- api/lovotts.js
+++ api/lovotts.js
@@ -1,4 +1,4 @@
-xport default async function handler(req, res) {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
